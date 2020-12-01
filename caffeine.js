2899e1e43c52490a56f81fbc795cf736454af4d7
Brb House on Fire <bobbywoj@sbcglobal.net>
2020-12-01 12:22:34 -0600

    added listener for finding key codes

--- caffeine.js
+++ caffeine.js
@@ -1,23 +1,24 @@
-const { command } = require("yargs");
-
-
-
-
 
 const commandKey = new KeyboardEvent("keypress", {
     // code: 91
     // code: "right-command key"
-    // code: "OSRight"
-    code: kVK_Command
-} as KeyboardEventInit);
+    code: "OSRight"
+    // code: 'kVK_Command'
+    // code: 'kVK_CapsLock'
+    // code: '0x003A'
+});// as KeyboardEventInit);
 
 function dis() {
-    console.log(commandKey);
-    console.log(commandKey.code);
+    // console.log(commandKey);
+    // console.log(commandKey.code);
     dispatchEvent(commandKey);
 }
+window.addEventListener("keydown", function(event) {
+    let str = "KeyboardEvent: key='" + event.key + "' | code='" +
+              event.code + "'";
+   
+    console.log(str);
+  }, true);
 
-// setInterval(commandKey, 3000);
-
-setInterval(dis(), 3000);
+// setInterval(dis, 2000);
 
