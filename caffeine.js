fb4ed841dd8644df3193b513de2653ad220ef84d
Brb House on Fire <bobbywoj@sbcglobal.net>
2020-12-01 12:10:09 -0600

    edit 1

--- caffeine.js
+++ caffeine.js
@@ -1,14 +1,23 @@
+const { command } = require("yargs");
 
 
 
 
 
 const commandKey = new KeyboardEvent("keypress", {
-    code: 91
+    // code: 91
+    // code: "right-command key"
+    // code: "OSRight"
+    code: kVK_Command
 } as KeyboardEventInit);
 
+function dis() {
+    console.log(commandKey);
+    console.log(commandKey.code);
+    dispatchEvent(commandKey);
+}
 
 // setInterval(commandKey, 3000);
 
-setInterval(dispatchEvent(commandKey), 3000);
+setInterval(dis(), 3000);
 
